9a0e748ad70ee0a6bf56c582ff6e48a0f55822ba
dagothar <dagothar@gmail.com>
2016-03-26 22:57:28 +0100

    sticky footer

--- src/main/java/todo/config/SecurityConfig.java
+++ src/main/java/todo/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                    .antMatchers("/css/**", "/js/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
